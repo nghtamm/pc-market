7b7f927143c55332f8921389baa02020fea1ccbb
durkyerunz <huyphuocnguyen.43@gmail.com>
2024-06-05 18:13:40 +0700

    add form nhanVien

---
+++ Forms/Form_NhanVien.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace pc_market.Forms
+{
+    public partial class Form_NhanVien : Form
+    {
+        public Form_NhanVien()
+        {
+            InitializeComponent();
+        }
+    }
+}
